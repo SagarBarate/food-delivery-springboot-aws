9f88d576f20598e278a69878c6ff5419cefcdcec
SagarBarate <sagarbarate2907@gmail.com>
2026-05-12 18:15:23 +0100

    Initial commit: add Eureka server Spring Boot project

---
+++ eureka/src/test/java/com/biteflow_springboot_aws/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biteflow_springboot_aws.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
